ac69a60520b3948fa5f53675c3f509094402b4e5
Roger Zelaya <rogerzelayaaaa@gmail.com>
2021-02-06 15:12:46 -0600

    Conversor de Unidades

--- CalculadoraFinal/app/src/main/java/com/example/calculadorafinal/MainActivity.java
+++ CalculadoraFinal/app/src/main/java/com/example/calculadorafinal/MainActivity.java
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabHost tbhConversores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tbhConversores = findViewById(R.id.tabConversores);
+        //tbhConversores = findViewById(R.id.tbhConversores);
+        tbhConversores.setup();
+
+        tbhConversores.addTab(tbhConversores.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("M"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("L"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("P"));
+
+
     }
     
 }
